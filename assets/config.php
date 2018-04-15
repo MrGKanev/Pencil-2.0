f27d98cf0d3c85a76dc667e74b4ade0eecae8f14
Gabriel Kanev <contact@mrgkanev.eu>
2018-04-15 10:29:40 +0300

    Revert "Yep still shit" - It wasn't for this project.
    This reverts commit 3c2daace3b012656fc4f369eaf6225800dfeac87.

---
+++ assets/config.php
@@ -0,0 +1,6 @@
+<?php
+$sitename = "Pencil 2.0";
+$description = "A Simple flat-file blog exercise built with plain(procedural) php, no funky frameworks, no fussy builds or installs. And, it comes with a super easy admin panel to edit, delete, create posts and upload images.";
+$author = "MrGKanev";
+$copyright = "github.com/MrGKanev/Pensil-2.0";
+?>
